c373eca321b10a2c150f1f08d47f39b93f8362ca
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2015-08-31 16:45:01 -0500

    Unified links in the header and footer

--- plugins/statics.py
+++ plugins/statics.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+PAGE_LINKS = [
+    {'url':'index.html', 'title':'Inicio'},
+    {'url':'about.html', 'title':'Nosotros'},
+    {'url':'challenge.html', 'title':'El Desafío'},
+    {'url':'leadership.html', 'title':'Programa de Liderazgo'},
+    {'url':'alumni.html', 'title':'Alumni'},
+    {'url':'allies.html', 'title':'Aliados'},
+]
 
 def preBuildPage(site, page, context, data):
     context['header_background'] = "assets/images/header-bg.jpg"
+    context['header_links'] = PAGE_LINKS
+    context['footer_links'] = [
+        PAGE_LINKS[:len(PAGE_LINKS)/2],
+        PAGE_LINKS[len(PAGE_LINKS)/2:],
+    ]
     return context, data
