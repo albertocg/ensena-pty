3a7d4d4b5ed94b3e2adddb0cae91a013968ea778
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2015-09-01 12:27:30 -0500

    Added blocks in about page. Added SCSS support

--- plugins/scss.py
+++ plugins/scss.py
@@ -21,16 +21,16 @@ except:
 	sys.exit("Could not find pyScss, please install: sudo easy_install pyScss")
 
 
-CSS_PATH = 'static/css'
+CSS_PATH = 'static/assets/css'
 
 for path in fileList(CSS_PATH):
-	
+
 	if not path.endswith('.scss'):
 		continue
-	
+
 	with open(path, 'r') as f:
 		data = f.read()
-	
+
 	css = Scss().compile(data)
 
 	with open(path.replace('.scss', '.css'), 'w') as f:
---
+++ plugins/staff_data.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+STAFF_DATA = [
+    {
+        'name': 'Lorena Valencia',
+        'photo': 'assets/images/staff/lorena_valencia.jpg' ,
+        'tagline': 'Directora Ejecutiva y Co-Fundadora',
+        'social_links': [
+            {'icon': 'fa-facebook', 'url': "#"},
+            {'icon': 'fa-twitter', 'url': "#"},
+            {'icon': 'fa-linkedin', 'url': "#"}
+        ]
+    },
+    {
+        'name': 'David Alexander Bernal Díaz',
+        'photo': 'assets/images/staff/david_bernal.jpg',
+        'tagline': 'Director de Reclutamiento, Selección y Colocación',
+        'social_links': [
+            {'icon': 'fa-facebook', 'url': "#"},
+            {'icon': 'fa-twitter', 'url': "#"},
+            {'icon': 'fa-linkedin', 'url': "#"}
+        ]
+    },
+    {
+        'name': 'Daniel McKee',
+        'photo': 'assets/images/staff/dan_mckee.jpg',
+        'tagline': 'Director de Formación y Apoyo',
+        'social_links': [
+            {'icon': 'fa-facebook', 'url': "#"},
+            {'icon': 'fa-twitter', 'url': "#"},
+            {'icon': 'fa-linkedin', 'url': "#"}
+        ]
+    },
+    {
+        'name': 'Julio Germán Arias Castillo',
+        'photo': 'assets/images/staff/julio_german.jpg',
+        'tagline': 'Director de Relaciones Públicas Internacionales',
+        'social_links': [
+            {'icon': 'fa-facebook', 'url': "#"},
+            {'icon': 'fa-twitter', 'url': "#"},
+            {'icon': 'fa-linkedin', 'url': "#"}
+        ]
+    },
+    {
+        'name': 'Marisa Arias',
+        'photo': 'assets/images/staff/david_bernal.jpg',
+        'tagline': 'Directora',
+        'social_links': [
+            {'icon': 'fa-facebook', 'url': "#"},
+            {'icon': 'fa-twitter', 'url': "#"},
+            {'icon': 'fa-linkedin', 'url': "#"}
+        ]
+    },
+    {
+        'name': 'Jorge Arosemena',
+        'photo': 'assets/images/staff/jorge_arosemena.jpg',
+        'tagline': 'Director',
+        'social_links': [
+            {'icon': 'fa-facebook', 'url': "#"},
+            {'icon': 'fa-twitter', 'url': "#"},
+            {'icon': 'fa-linkedin', 'url': "#"}
+        ]
+    },
+    {
+        'name': 'Julio Escobar Villarué',
+        'photo': 'assets/images/staff/julio_escobar.jpg',
+        'tagline': 'Asesor',
+        'social_links': [
+            {'icon': 'fa-facebook', 'url': "#"},
+            {'icon': 'fa-twitter', 'url': "#"},
+            {'icon': 'fa-linkedin', 'url': "#"}
+        ]
+    },
+]
+
+
+def preBuildPage(site, page, context, data):
+    context['staff_data'] = STAFF_DATA
+    return context, data
